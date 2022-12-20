e9320f0e9a3d289a8d74e95b7748abdf3ffb16d5
yazidi <yazidi17@gmail.com>
2022-12-20 21:14:56 +0100

    first commit

---
+++ disconnectedModeActivite3/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace disconnectedModeActivite3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdPers = new System.Windows.Forms.DataGridView();
+            this.grdPersPass = new System.Windows.Forms.DataGridView();
+            this.grdPass = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grdPers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdPersPass)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdPass)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdPers
+            // 
+            this.grdPers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdPers.Location = new System.Drawing.Point(30, 40);
+            this.grdPers.Name = "grdPers";
+            this.grdPers.Size = new System.Drawing.Size(216, 300);
+            this.grdPers.TabIndex = 0;
+            // 
+            // grdPersPass
+            // 
+            this.grdPersPass.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdPersPass.Location = new System.Drawing.Point(305, 40);
+            this.grdPersPass.Name = "grdPersPass";
+            this.grdPersPass.Size = new System.Drawing.Size(202, 300);
+            this.grdPersPass.TabIndex = 1;
+            // 
+            // grdPass
+            // 
+            this.grdPass.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdPass.Location = new System.Drawing.Point(587, 40);
+            this.grdPass.Name = "grdPass";
+            this.grdPass.Size = new System.Drawing.Size(187, 300);
+            this.grdPass.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 409);
+            this.Controls.Add(this.grdPass);
+            this.Controls.Add(this.grdPersPass);
+            this.Controls.Add(this.grdPers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdPers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdPersPass)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdPass)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdPers;
+        private System.Windows.Forms.DataGridView grdPersPass;
+        private System.Windows.Forms.DataGridView grdPass;
+    }
+}
+
---
+++ disconnectedModeActivite3/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace disconnectedModeActivite3
+{
+    public partial class Form1 : Form
+    {
+        private DataSet MonData = new DataSet("MonData");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Permet de créer une colonne et ajouter a MaTable
+        private void addCol(
+            DataTable MaTable,
+            string nom,
+            string Coltype,
+            Boolean Index = false
+            )
+        {
+        DataColumn dc = new DataColumn(nom, Type.GetType(Coltype));
+         if(Index)
+         {
+                dc.Unique = true;
+                dc.AutoIncrement = true;
+                dc.AllowDBNull = false;
+                dc.ReadOnly = true;
+         }
+            MaTable.Columns.Add(dc);
+        }
+
+        //ajouter des lignes à la table Passion
+        private void addRow(
+            DataTable MaTable,
+            string[] passions
+            )
+        {
+            DataRow row;//Reference
+            foreach(string passion in passions)
+            {
+                row = MaTable.NewRow();
+                row["Passion"] = passion;
+                //Ajout
+
+                MaTable.Rows.Add(row);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Creer et definition de la table Personnes
+            DataTable table = new DataTable("Personnes");
+            addCol(table, "IndexNom", "System.Int16", true);
+            addCol(table, "Prenom", "System.String");
+            addCol(table, "Nom", "System.String");
+            table.PrimaryKey = new DataColumn[] {
+               table.Columns["IndexNom"]
+           };
+
+            ArrayList Maliste = new ArrayList();
+            Maliste.Add(new object[] { "Jean", "Bon"});
+            Maliste.Add(new object[] { "Pierre", "Ade" });
+            Maliste.Add(new object[] { "Jacques", "Adis" });
+            Maliste.Add(new object[] { "Leon", "Nard" });
+            Maliste.Add(new object[] { "Jean", "Tand" });
+            
+
+            foreach(object[] obj in Maliste)
+            {
+                DataRow row = table.NewRow();
+                row["Prenom"] = obj[0];
+                row["Nom"] = obj[1];
+
+                table.Rows.Add(row);
+            }
+
+            //Ajouter le Datatable au Dataset
+            MonData.Tables.Add(table);
+            grdPers.DataSource = MonData;
+            grdPers.DataMember = "Personnes";
+
+            //Creation de la table Passion
+
+            table = new DataTable("Passions");
+            addCol(table, "IndexPassion", "System.Int16", true);
+            addCol(table, "Passion", "System.String");
+            table.PrimaryKey = new DataColumn[] {
+               table.Columns["IndexPassion"]
+           };
+
+            addRow(table, 
+                new string[] { "ski", "marche", "lecture", "philatelie","informatique" });
+
+            //Ajouter le Datatable au Dataset
+            MonData.Tables.Add(table);
+            grdPass.DataSource = MonData;
+            grdPass.DataMember = "Passions";
+
+            //créer la table PressPass
+            table = new DataTable("PersPass");
+            addCol(table, "IndexNom", "System.Int16");
+            addCol(table, "IndexPassion", "System.Int16");
+
+            //Ajouter le DataTable au Dataset
+            MonData.Tables.Add(table);
+            grdPersPass.DataSource = MonData;
+            grdPersPass.DataMember = "PersPass";
+
+            //Definir la cle de la table
+            /* table.PrimaryKey = new DataColumn[] {
+                 table.Columns[0],table.Columns[1]
+             };*/
+            table.PrimaryKey = new DataColumn[] {
+               table.Columns["IndexNom"],table.Columns["IndexPassion"]
+           };
+
+            Random r = new Random();
+            short c, MaxPers, MaxPass;
+
+            MaxPers = (short)MonData.Tables["Personnes"].Rows.Count;
+            MaxPass = (short)MonData.Tables["Passions"].Rows.Count;
+
+            for(c=0;c<=12;c++ )
+            {
+                DataRow row = table.NewRow();
+
+                row.ItemArray = 
+                    new object[] { r.Next(MaxPers),r.Next(MaxPass)};
+
+                try
+                {
+                    table.Rows.Add(row);
+                }
+                catch(Exception)
+                {
+                    //Violation de cles primaire
+                }
+            }
+
+            //Declaration des relations 
+
+            DataRelation rel =
+                new DataRelation(
+                    "PersInt",
+                    MonData.Tables["Personnes"].Columns["IndexNom"],
+                    MonData.Tables["PersPass"].Columns["IndexNom"]
+                );
+
+            MonData.Relations.Add(rel);
+
+            rel =
+                new DataRelation(
+                    "PassInt",
+                    MonData.Tables["Passions"].Columns["IndexPassion"],
+                    MonData.Tables["PersPass"].Columns["IndexPassion"]
+                );
+
+            MonData.Relations.Add(rel);
+
+        }
+    }
+}
